c25e57a0218dfcaaaaad3157e8c854886087ca2a
Sanvi Rahman <SanviRahman>
2025-05-25 16:52:17 +0600

    first commit

---
+++ JS_PPS/numberTostring.js
@@ -0,0 +1,13 @@
+let n=12345;
+let num=" ";
+
+function numberToString(num)
+{
+    return num+' ';
+}
+
+console.log(numberToString(n));
+console.log(typeof(num));
+
+
+
---
+++ JS_PPS/stringTonumber.js
@@ -0,0 +1,10 @@
+let str="2345";
+let a=1;
+
+function stringToNumber(a)
+{
+    return a+=str;
+}
+
+console.log(stringToNumber(a));
+console.log(typeof(a));
