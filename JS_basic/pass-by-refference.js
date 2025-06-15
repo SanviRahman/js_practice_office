bcaa206f0beb8165c1ff288cabc63e417f18fff7
Sanvi Rahman <SanviRahman>
2025-06-15 16:12:17 +0600

    last commit

---
+++ JS_basic/pass-by-refference.js
@@ -0,0 +1,10 @@
+var obj={
+    b:20
+}
+function myFunc(obj)
+{
+    obj.b+=100;
+    console.log(obj.b);//120
+}
+myFunc(obj);
+console.log(obj.b);//120
---
+++ JS_basic/pass-by-value.js
@@ -0,0 +1,9 @@
+var a=10;
+
+function func(a)
+{
+    a+=a;
+    console.log(a)//20
+}
+func(a);
+console.log(a);//10
