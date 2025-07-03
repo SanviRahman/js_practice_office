1724dbedc4d9ae9f7d692738c8117fcbb5230769
Sanvi Rahman <SanviRahman>
2025-07-03 11:07:10 +0600

    Completed continue statement

---
+++ continue/check i+j.js
@@ -0,0 +1,8 @@
+for(let i=1; i<4;i++){
+    for(let j=1; j<4; j++){
+        if(i+j==3){
+            continue;
+        }
+        console.log(i,j);
+    }
+}
---
+++ continue/continue even number.js
@@ -0,0 +1,16 @@
+// let i=0; 
+// while(i<=10){
+//     if(i%2==1){
+//          i++;
+//         continue; 
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+for(let i=0; i<=10; i++){
+    if(i%2==1){
+        continue;
+    }
+    console.log(i);
+}
