942fa0340736dc140aa3662ceaaf0e634c3078c6
Sanvi Rahman <SanviRahman>
2025-07-03 15:32:14 +0600

    Object destructruing completed

---
+++ Hoisting/hoisting_1.js
@@ -0,0 +1,9 @@
+let res=add(10,20);
+console.log(res);
+
+function add(a,b){
+  let sum=0;
+  sum=a+b;
+  return sum;
+}
+
---
+++ Object destructuring/ob destructuring.js
@@ -0,0 +1,7 @@
+let person={
+    firstName:"Sanvi",
+    lastName:"Rahman"
+};
+
+let {firstName:fName,lastName:lname}=person;
+console.log(fName+" "+lname);
