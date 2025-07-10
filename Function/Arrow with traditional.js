496694850194fd4646aef6df2ad6b347cd077b38
Sanvi Rahman <SanviRahman>
2025-07-10 14:02:54 +0600

    understand this keyword's behaviour

--- Function/Arrow with traditional.js
+++ Function/Arrow with traditional.js
@@ -1,10 +1,10 @@
 function Counter() {
     this.count = 0;
 
-    setInterval(() => {
-        this.count++; 
+    setTimeout(function(){
+        this.count++;
         console.log(this.count);
-    }, 1000);
+    },1000);
 }
 
 const c = new Counter();
